2bfba71626386757d537b4162d4426ac020ed020
chrisngili <ngilichristopher@gmail.com>
2025-05-19 14:38:02 +0300

    all tests passed

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Mr.Chocolate", 12, "M", "Brown", "Choleric",True);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Rowdy", 19, "M", "Red", "Sanguine",False);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Tabitha", 2, "F", "Brown", "Choleric",True);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 13, "F", "Orange", "Melancholic",False);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Melissa", 23, "F", "Turqoise", "Choleric",True);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Grinch", 62, "M", "Brown", "Choleric",True);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Wendy", 17, "F", "Brown", "Choleric",True);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (NULL, 12, "M", "Brown", "Choleric",True);
